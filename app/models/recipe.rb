02cc2134d4be365ce582cf176b8377b5930b4f95
Zuri Hunter <zurihunter92@gmail.com>
2015-06-16 23:28:19 +0000

    Forms, Recipes, Directions, Ingredients, Images

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,16 @@
 class Recipe < ActiveRecord::Base
+  has_many :ingredients
+  has_many :directions
+  #sets up the nested form for cocoon gem to draw the correlation between ingredients/directions with recipe
+  
+  
+  accepts_nested_attributes_for :ingredients, :reject_if => :all_blank, :allow_destroy => true
+  accepts_nested_attributes_for :directions, :reject_if => :all_blank, :allow_destroy => true
+  
   has_attached_file :image, styles: { :medium => "400x400#" }
   #the hash tag by the pixel size for the image tells the gem to crop the image
+  
+  validates :title, :description, :image, presence: true
+  #only acceptes if these attributes have been filled by user
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
